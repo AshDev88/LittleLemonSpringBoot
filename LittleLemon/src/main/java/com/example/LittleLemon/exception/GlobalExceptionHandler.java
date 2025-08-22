eab87cae3d75db6fbcea29a74de17bc478b92555
AshDev88 <ashishdhavale@live.com>
2025-08-22 21:28:21 +0530

    Initial commit-Category Exception

---
+++ LittleLemon/src/main/java/com/example/LittleLemon/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.LittleLemon.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex){
+      ErrorResponse error = new ErrorResponse("NOT_FOUND",ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneric(Exception ex){
+        ErrorResponse error = new ErrorResponse( "INTERNAL_ERROR","SOMETHING WENT WRONG");
+        return  new ResponseEntity<>(error,HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+}
